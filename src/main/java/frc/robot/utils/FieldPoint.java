5b648e4fa60f4e21c2d954651428e9dd1ded238c
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2025-01-11 16:20:59 -0500

    Setup FieldPoints

--- src/main/java/frc/robot/utils/FieldPoint.java
+++ src/main/java/frc/robot/utils/FieldPoint.java
@@ -4,6 +4,24 @@
 
 package frc.robot.utils;
 
+import com.chaos131.poses.MirroredDrivePose;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 /** Add your docs here. */
-public class FieldPoint {
+public class FieldPoint extends MirroredDrivePose {
+
+    public static final FieldPoint processor=new FieldPoint("processor", new Pose2d(5.988,0,Rotation2d.fromDegrees(90)));
+
+    public FieldPoint (String name, Pose2d pose ) {
+        super(17.524,Alliance.Blue, name, pose);
+        makeredpose();
+    }
+
+    private void makeredpose (){
+
+    //
+    }
 }
