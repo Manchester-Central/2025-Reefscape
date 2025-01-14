4ab3618ed93df14e09f75c02337b07bd9312fb14
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2025-01-13 20:14:26 -0500

    Delta Saved us field rotation by chichi & harley

--- src/main/java/frc/robot/utils/FieldPoint.java
+++ src/main/java/frc/robot/utils/FieldPoint.java
@@ -8,20 +8,48 @@ import com.chaos131.poses.MirroredDrivePose;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 /** Add your docs here. */
-public class FieldPoint extends MirroredDrivePose {
+public class FieldPoint {
+     /** The pre-calculated red pose */
+  protected final Pose2d m_redPose;
 
-    public static final FieldPoint processor=new FieldPoint("processor", new Pose2d(5.988,0,Rotation2d.fromDegrees(90)));
+  /** The pre-calculated blue pose */
+  protected final Pose2d m_bluePose;
 
-    public FieldPoint (String name, Pose2d pose ) {
-        super(17.524,Alliance.Blue, name, pose);
-        makeredpose();
+  /** A potentially null name for the pose */
+  protected final String m_name;
+
+  /** The corresponding alliance the original pose was built for */
+  protected final Alliance m_defaultAlliance;
+
+  protected final double fieldLength = 17.524;
+
+  protected final double fieldWidth = 8.052;
+    public static final FieldPoint processor = new FieldPoint("processor", new Pose2d(5.988,0,Rotation2d.fromDegrees(90)));
+    public static final FieldPoint leftSource = new FieldPoint("leftSource", new Pose2d(16.685,7.398,Rotation2d.fromDegrees(90)));
+    public static final FieldPoint rightSource = new FieldPoint("rightSource", new Pose2d(16.685,0.6553,Rotation2d.fromDegrees(90)));
+
+    public FieldPoint (String name, Pose2d pose) {
+        m_name = name;
+        m_bluePose = pose;
+        m_defaultAlliance = Alliance.Blue;
+        Translation2d pTranslation2d = pose.getTranslation().minus(new Translation2d(fieldLength, fieldWidth).div(2));
+        pTranslation2d = pTranslation2d.rotateBy(Rotation2d.fromDegrees(180));
+        pTranslation2d = pTranslation2d.plus(new Translation2d(fieldLength, fieldWidth).div(2));
+        m_redPose = new Pose2d(pTranslation2d, pose.getRotation().plus(Rotation2d.fromDegrees(180)));
+
     }
 
-    private void makeredpose (){
+    public Pose2d getBluePose() {
 
-    //
+        return m_bluePose;
+    }
+
+    public Pose2d getRedPose() {
+
+        return m_redPose;
     }
 }
