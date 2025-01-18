ed040f27741dd134e3f91c7811997a7e70653efc
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2025-01-18 14:06:21 -0500

    Starting to add pivot

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -4,9 +4,11 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.util.Color8Bit;
@@ -17,25 +19,39 @@ public class Lift extends SubsystemBase {
   private MechanismLigament2d m_mechanism2dLift;
 
   // Simulator
-  private DCMotor m_liftGearBox = DCMotor.getVex775Pro(4);
-  private ElevatorSim m_sim =
-      new ElevatorSim(m_liftGearBox, 10, 4, Units.inchesToMeters(2), 0, 1.5, false, 0, 0.005, 0);
+  // TODO: add values to numMotors
+  private DCMotor m_pivotGearBox = DCMotor.getVex775Pro(4);
+  private SingleJointedArmSim m_pivotSim =
+      new SingleJointedArmSim(
+          m_pivotGearBox,
+          10.0,
+          4.0,
+          2.0,
+          Rotation2d.fromDegrees(30).getRadians(),
+          Rotation2d.fromDegrees(75).getRadians(),
+          true,
+          Rotation2d.fromDegrees(35).getRadians());
+  private DCMotor m_elevatorGearBox = DCMotor.getVex775Pro(4);
+
+  private ElevatorSim m_elevatorSim =
+      new ElevatorSim(
+          m_elevatorGearBox, 10, 4, Units.inchesToMeters(2), 0, 1.5, false, 0, 0.005, 0);
 
   /** Creates a new Lift. */
   public Lift(MechanismRoot2d mechanismRoot) {
     m_mechanism2dLift =
         mechanismRoot.append(
             new MechanismLigament2d(
-                "lift", m_sim.getPositionMeters(), 45, 6, new Color8Bit(0, 255, 0)));
+                "lift", m_elevatorSim.getPositionMeters(), 45, 6, new Color8Bit(0, 255, 0)));
   }
 
   @Override
   public void periodic() {
-    m_sim.update(0.02);
+    m_elevatorSim.update(0.02);
   }
 
   public void setSpeed(double speed) {
-    m_sim.setInput(speed);
-    m_mechanism2dLift.setLength(m_sim.getPositionMeters());
+    m_elevatorSim.setInput(speed);
+    m_mechanism2dLift.setLength(m_elevatorSim.getPositionMeters());
   }
 }
