7c58a4f67c1f24cf8ed3088d5a9c0e8bcfb3a1fd
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2025-01-21 20:28:24 -0500

    STATE MACHINE

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,14 +4,42 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.shared.ISubsystemState;
+import frc.robot.subsystems.shared.StateBasedSubsystem;
 
-public class Intake extends SubsystemBase {
+public class Intake extends StateBasedSubsystem<Intake.IntakeState> {
+  public enum IntakeState implements ISubsystemState {
+    START,
+    STOW,
+    DEPLOY;
+  };
+
   /** Creates a new Intake. */
-  public Intake() {}
+  public Intake() {
+    super(IntakeState.START);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  @Override
+  protected void runStateMachine() {
+    System.out.println(m_currentState);
+    switch (m_currentState) {
+      case START:
+        m_currentState = IntakeState.STOW;
+        break;
+
+      case STOW:
+        break;
+
+      case DEPLOY:
+        break;
+
+      default:
+        break;
+    }
   }
 }
---
+++ src/main/java/frc/robot/subsystems/shared/StateBasedSubsystem.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shared;
+
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public abstract class StateBasedSubsystem<TState extends ISubsystemState> extends SubsystemBase {
+  protected TState m_currentState;
+
+  protected StateBasedSubsystem(TState startState) {
+    m_currentState = startState;
+    this.setDefaultCommand(new RunCommand(() -> runStateMachine(), this));
+  }
+
+  protected abstract void runStateMachine();
+}
