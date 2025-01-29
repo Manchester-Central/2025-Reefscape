96f3fefc71094e44409826e6bc206aa14b3791d6
CHAOS 131 <131.chaos@gmail.com>
2025-01-28 19:42:24 -0500

    GripperPivot realistic movement

--- src/main/java/frc/robot/subsystems/lift/GripperPivot.java
+++ src/main/java/frc/robot/subsystems/lift/GripperPivot.java
@@ -4,18 +4,65 @@
 
 package frc.robot.subsystems.lift;
 
+import com.chaos131.pid.PIDFValue;
+import com.chaos131.pid.PIDTuner;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.ChaosTalonFx;
 
 /** Add your docs here. */
 public class GripperPivot extends SubsystemBase {
-  private Rotation2d m_targetAngle = Rotation2d.fromDegrees(0);
+  private double kGearRatio = 100.0;
+  private double kJkgMetersSquared = 0.1;
+  private Rotation2d m_targetAngle = Rotation2d.fromDegrees(120);
+  private DCMotor m_dcMotor = DCMotor.getKrakenX60(1);
+  private DCMotorSim m_motorSim =
+      new DCMotorSim(
+          LinearSystemId.createDCMotorSystem(m_dcMotor, kJkgMetersSquared, kGearRatio),
+          m_dcMotor,
+          0.001,
+          0.001);
+  private ChaosTalonFx m_motor = new ChaosTalonFx(3, kGearRatio, m_motorSim);
+  private PIDTuner m_pidTuner = new PIDTuner("GripperPivot", true, 5.0, 0.001, 0.0, this::tunePID);
+
+  public GripperPivot() {
+    m_motor.Configuration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    m_motor.Configuration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    m_motor.Configuration.CurrentLimits.SupplyCurrentLimitEnable = true;
+    m_motor.Configuration.CurrentLimits.SupplyCurrentLimit = 40;
+    m_motor.Configuration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    m_motor.Configuration.Feedback.SensorToMechanismRatio = 10; // TODO: get real value
+    m_motor.Configuration.ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.1;
+    m_motor.applyConfig();
+  }
+
+  public void tunePID(PIDFValue pidfValue) {
+    m_motor.tunePID(pidfValue, 0.0);
+  }
 
   public void setTargetAngle(Rotation2d newAngle) {
     m_targetAngle = newAngle;
+    m_motor.moveToPosition(newAngle.getDegrees());
   }
 
   public Rotation2d getCurrentAngle() {
-    return m_targetAngle; // TODO get actual motor angle
+    return Rotation2d.fromDegrees(
+        m_motor.getPosition().getValueAsDouble()); // TODO get actual motor angle
+  }
+
+  @Override
+  public void periodic() {
+    m_pidTuner.tune();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    m_motor.simUpdate();
   }
 }
