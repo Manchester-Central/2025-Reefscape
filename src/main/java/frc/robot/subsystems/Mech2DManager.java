da042285d417632ac8af929d4cc5cc09c7606e15
CHAOS 131 <131.chaos@gmail.com>
2025-02-08 14:53:47 -0500

    Moved Mech2d from Smartboard to Advantage Kit

--- src/main/java/frc/robot/subsystems/Mech2DManager.java
+++ src/main/java/frc/robot/subsystems/Mech2DManager.java
@@ -4,22 +4,23 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.lift.IdLift;
 import frc.robot.subsystems.lift.IdLift.IdLiftValues;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 /** Add your docs here. */
 public class Mech2DManager extends SubsystemBase {
-  // @AutoLogOutput(key = "Manipulator")
-  private Mechanism2d m_liftBase;
-  private MechanismRoot2d m_liftRoot;
-  private MechanismLigament2d m_extenderLigament;
-  private MechanismLigament2d m_gripperLigament;
+  @AutoLogOutput(key = "Mech2d/Lift")
+  private LoggedMechanism2d m_liftBase;
+
+  private LoggedMechanismRoot2d m_liftRoot;
+  private LoggedMechanismLigament2d m_extenderLigament;
+  private LoggedMechanismLigament2d m_gripperLigament;
   private IdLift m_idLift;
 
   private final Color8Bit kExtenderColor = new Color8Bit(0, 0, 255);
@@ -27,10 +28,12 @@ public class Mech2DManager extends SubsystemBase {
   private final Color8Bit kGripperForward = new Color8Bit(0, 255, 0);
   private final Color8Bit kGripperReverse = new Color8Bit(255, 0, 0);
 
-  private Mechanism2d m_intakeBase;
-  private MechanismRoot2d m_intakeRoot;
-  private MechanismLigament2d m_innerIntakeLigament;
-  private MechanismLigament2d m_outerIntakeLigament;
+  @AutoLogOutput(key = "Mech2d/Intake")
+  private LoggedMechanism2d m_intakeBase;
+
+  private LoggedMechanismRoot2d m_intakeRoot;
+  private LoggedMechanismLigament2d m_innerIntakeLigament;
+  private LoggedMechanismLigament2d m_outerIntakeLigament;
   private Intake m_intake;
 
   private final Color8Bit kInnerIntakeColor = new Color8Bit(255, 0, 255);
@@ -42,22 +45,24 @@ public class Mech2DManager extends SubsystemBase {
     m_idLift = idLift;
     m_intake = intake;
 
-    m_liftBase = new Mechanism2d(2, 3);
+    m_liftBase = new LoggedMechanism2d(2, 3);
     m_liftRoot = m_liftBase.getRoot("Lift", 0.8, 0.2);
     m_extenderLigament =
-        m_liftRoot.append(new MechanismLigament2d("Extender", 0, 0, 8, kExtenderColor));
+        m_liftRoot.append(new LoggedMechanismLigament2d("Extender", 0, 0, 8, kExtenderColor));
     m_gripperLigament =
-        m_extenderLigament.append(new MechanismLigament2d("Gripper", 0.2, 0, 10, kGripperNeutral));
-    SmartDashboard.putData("Mech2d/Lift", m_liftBase);
+        m_extenderLigament.append(
+            new LoggedMechanismLigament2d("Gripper", 0.2, 0, 10, kGripperNeutral));
+    // SmartDashboard.putData("Mech2d/Lift", m_liftBase);
 
-    m_intakeBase = new Mechanism2d(2, 3);
+    m_intakeBase = new LoggedMechanism2d(2, 3);
     m_intakeRoot = m_intakeBase.getRoot("Intake", 1.2, 0.2);
     m_innerIntakeLigament =
-        m_intakeRoot.append(new MechanismLigament2d("InnerIntake", 0.3, 90, 8, kInnerIntakeColor));
+        m_intakeRoot.append(
+            new LoggedMechanismLigament2d("InnerIntake", 0.3, 90, 8, kInnerIntakeColor));
     m_outerIntakeLigament =
         m_innerIntakeLigament.append(
-            new MechanismLigament2d("OuterIntake", 0.2, -90, 10, kIntakeNeutralColor));
-    SmartDashboard.putData("Mech2d/Intake", m_intakeBase);
+            new LoggedMechanismLigament2d("OuterIntake", 0.2, -90, 10, kIntakeNeutralColor));
+    // Logger.recordOutput("Mech2d/Intake", m_intakeBase);
   }
 
   @Override
